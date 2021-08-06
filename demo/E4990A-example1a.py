b50e56838c31ddb015a0e13733c53255dc3fd108
Michael Hayes <michael.hayes@canterbury.ac.nz>
2021-08-06 17:43:25 +1200

    Add style file

--- demo/E4990A-example1a.py
+++ demo/E4990A-example1a.py
@@ -6,13 +6,9 @@ from zfitter import Plotter
 from zfitter import impedancedata
 from zfitter import Parallel2SeriesRLModel
 
-#style.use('z.mplstyle')
+style.use('z.mplstyle')
 
 data = impedancedata('E4990A-example1.csv')
-
-rangesRL2 = ((60e-3, 90e-3), (8e-3, 12e-3),
-             (15, 80), (10e-3, 30e-3),
-             (1, 80), (1e-12, 30e-3))
 
 rangesRL2 = ((60e-3, 90e-3), (8e-3, 12e-3),
              (15, 80), (10e-3, 30e-3))
@@ -20,8 +16,6 @@ rangesRL2 = ((60e-3, 90e-3), (8e-3, 12e-3),
 zfitter = ZFitter(Parallel2SeriesRLModel, data.f, data.Z)
 
 fitmodel = zfitter(ranges=rangesRL2)
-
-Z = fitmodel.Z(data.f)
 
 plotter = Plotter()
 plotter.Z_error(data, fitmodel)
