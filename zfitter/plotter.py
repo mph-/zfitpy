b6e2e2f30cb0f7fce2cc8bba1fd49faced1d04a9
Michael Hayes <michael.hayes@canterbury.ac.nz>
2021-08-08 13:39:48 +1200

    Add magphase option

--- zfitter/plotter.py
+++ zfitter/plotter.py
@@ -1,4 +1,5 @@
 from matplotlib.pyplot import subplots
+from numpy import degrees, angle
 
 class Plotter(object):
 
@@ -21,18 +22,29 @@ class Plotter(object):
         axes.set_title(title)
         axes.legend()
 
-    def Z_fit(self, data, model, axes=None, title=None):
+    def Z_fit(self, data, model, axes=None, title=None, magphase=False):
 
         Z = model.Z(data.f)
         
         if axes is None:
             fig, axes = subplots(1)
 
-        axes.plot(data.f, data.Z.real, label=data.name + ' data real')
-        axes.plot(data.f, data.Z.imag, '--', label=data.name + ' data imag')
+        if magphase:
+            ax2 = axes.twinx()
 
-        axes.plot(data.f, Z.real, label=data.name + ' fit real')
-        axes.plot(data.f, Z.imag, '--', label=data.name + ' fit imag')        
+            axes.plot(data.f, abs(data.Z), label=data.name + ' data mag')
+            ax2.plot(data.f, degrees(angle(data.Z)), '--', label=data.name + ' data phase')
+            axes.plot(data.f, abs(Z), label=data.name + ' fit mag')
+            ax2.plot(data.f, degrees(angle(Z)), '--', label=data.name + ' fit phase')                     
+            ax2.legend()
+            ax2.set_ylabel('Impedance phase (degrees)')            
+            
+        else:
+            axes.plot(data.f, data.Z.real, label=data.name + ' data real')
+            axes.plot(data.f, data.Z.imag, '--', label=data.name + ' data imag')
+
+            axes.plot(data.f, Z.real, label=data.name + ' fit real')
+            axes.plot(data.f, Z.imag, '--', label=data.name + ' fit imag')
 
         axes.set_xlabel('Frequency (Hz)')
         axes.set_ylabel('Impedance (ohms)')
