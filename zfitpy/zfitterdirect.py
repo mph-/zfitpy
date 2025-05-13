843f3e6dd9c0af0f55544615fb0da72d1725dfa2
Michael Hayes <michael.hayes@canterbury.ac.nz>
2025-05-14 09:19:55 +1200

    Fix admittance fitting for direct

--- zfitpy/zfitterdirect.py
+++ zfitpy/zfitterdirect.py
@@ -46,6 +46,6 @@ class ZFitterDirect(ZFitterBase):
         if opt == 'Z':
             rmse = model.Zrmse(self.f, self.Z)
         else:
-            rmse = model.Yrmse(self.f, self.Z)
+            rmse = model.Yrmse(self.f, self.Y)
 
         return model, rmse, None
